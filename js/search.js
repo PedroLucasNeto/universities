445bf9a3312f2ecdf85182f21c5eb5556d3cb3b8
Pedro Lucas <101678622+PedroLucasNeto@users.noreply.github.com>
2024-04-18 11:03:06 -0300

    feat: listing universities

--- js/search.js
+++ js/search.js
@@ -1,62 +1,70 @@
-$(document).ready()
+$(document).ready(() => {
 
-const urlBase = "http://universities.hipolabs.com/search"
+  const urlBase = "http://universities.hipolabs.com/search"
 
-function searchUniversities(country, university){
-  let url = urlBase;
-  if(country && university){
-    url += `?country=${country}&name=${university}`
-  }else if ( country && !university){
-    url += `?country=${country}`
-  }else if ( !country && university){
-    url += `?name=${university}`
+  function searchUniversities(country, university, callback) {
+    let url = urlBase;
+    if (country && university) {
+      url += `?country=${country}&name=${university}`
+    } else if (country && !university) {
+      url += `?country=${country}`
+    } else if (!country && university) {
+      url += `?name=${university}`
+    }
+
+    $.ajax({
+      url: url,
+      type: 'GET'
+    })
+      .done((data) => {
+        callback(data); // Chamando a função de retorno de chamada com os dados recebidos
+      })
   }
 
-  console.log('URL'. url);
-  
-    $.ajax({
-        url: url,
-        type: 'GET'
-    })
-    .done((data)=>{ 
-        const universities = data;
-        return universities;
-    })
-}
-
-function createRow(university){
-    const link = `<a href="${university.web_pages[0]}" class="btn btn-primary">Go to university</a>`
+  function createRow(university) {
+    const link = `<a href="${university.web_pages[0]}" class="btn btn-primary" target="_blank">Go to university</a>`
     return `
-    <tr>
-        <td>${university.name}</td>
-        <td>${university.country}</td>
-        <td>${link}</td>
-    </tr>
-        `
-  
-}
-
-$('#search-universities').click(() => {
-    const country = $('search-by-country').val()
-    const university = $('search-by-university').val()
+      <tr>
+          <td>${university.name}</td>
+          <td>${university.country}</td>
+          <td>${link}</td>
+      </tr>
+          `
+  }
+  $('#search-universities').click(() => {
+    const country = $('#country-name').val()
+    const university = $('#university-name').val()
     const tableBody = $('#table-body');
+  
+    searchUniversities(country, university, (results) => {
+      if (results.length <= 0) {
+        alert('No registries were found');
+        return;
+      } else {
+        let items = [];
+        let counter = 0;
+        let index = 0;
+  
+        while (counter < 20 && index < results.length) {
+          const universityResult = results[index];
+          const row = createRow(universityResult);
+          items.push(row);
+          counter++;
+          index++;
+        }
+  
+        tableBody.html(items.join('')); // Usando join sem nenhum delimitador para concatenar os elementos do array em uma string
+  
+        console.log(results);
+        
+        // Navegar até a tabela de resultados
+        const tableElement = document.getElementById('table-body');
+        if (tableElement) {
+          tableElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  });
+  
 
-    const results = searchUniversities(country, university)
-
-    console.log(results);
-    if(results.length<=0){
-      alert('No registries were found');
-      return;
-    }else {
-          let items = [];
-      
-           results.map((universityResult) => {
-              const row = createRow(universityResult)
-              items.append(row)
-          } )
-      
-          tableBody.html(items);
-
-    }
-    
-})
+});
